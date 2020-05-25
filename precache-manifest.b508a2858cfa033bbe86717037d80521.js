89229887620da04d7689beae05d5fb1bd1e5c424
Mateusz Wasil <wasil.mateusz@gmail.com>
2020-05-25 13:31:56 +0200

    Updates

---
+++ precache-manifest.b508a2858cfa033bbe86717037d80521.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "697213e2680107ea38ae33d04cb41b46",
+    "url": "/myPortfolio-react/index.html"
+  },
+  {
+    "revision": "8a0242c3e3110e6811f1",
+    "url": "/myPortfolio-react/static/css/main.3448be05.chunk.css"
+  },
+  {
+    "revision": "5d4fb92c6df05dae0843",
+    "url": "/myPortfolio-react/static/js/2.79122431.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myPortfolio-react/static/js/2.79122431.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a0242c3e3110e6811f1",
+    "url": "/myPortfolio-react/static/js/main.7c1a8f04.chunk.js"
+  },
+  {
+    "revision": "9cd3ddf89e1f21c39076",
+    "url": "/myPortfolio-react/static/js/runtime-main.51e6720d.js"
+  },
+  {
+    "revision": "00a6d71ecd16beab4cbc5c6611671fda",
+    "url": "/myPortfolio-react/static/media/angular.00a6d71e.png"
+  },
+  {
+    "revision": "5660ee2159477aa4995ab11bb173da20",
+    "url": "/myPortfolio-react/static/media/blog.5660ee21.JPG"
+  },
+  {
+    "revision": "a9225eef34495495f7f4ca8032f15018",
+    "url": "/myPortfolio-react/static/media/bootstrap.a9225eef.png"
+  },
+  {
+    "revision": "c9c2cf81907759c0c414e167fa6e6199",
+    "url": "/myPortfolio-react/static/media/css3.c9c2cf81.png"
+  },
+  {
+    "revision": "a5c318381f4ab1afd72393020b6ec6bf",
+    "url": "/myPortfolio-react/static/media/email-icon.a5c31838.png"
+  },
+  {
+    "revision": "17a6652f345bc5ea2db2d4fba0c95a84",
+    "url": "/myPortfolio-react/static/media/git-icon.17a6652f.png"
+  },
+  {
+    "revision": "27dd939575e9d3d678cccdffdf0ef2d6",
+    "url": "/myPortfolio-react/static/media/git.27dd9395.png"
+  },
+  {
+    "revision": "f2282f94c041c37ef4db028e428e41c1",
+    "url": "/myPortfolio-react/static/media/html5.f2282f94.png"
+  },
+  {
+    "revision": "90eaaab2e6081c892e2474d4f7fa4de7",
+    "url": "/myPortfolio-react/static/media/js2.90eaaab2.png"
+  },
+  {
+    "revision": "8d63f77b7a21ff46739294496697045f",
+    "url": "/myPortfolio-react/static/media/linkedin-icon.8d63f77b.png"
+  },
+  {
+    "revision": "1f51812f56da377531283af99af18cbc",
+    "url": "/myPortfolio-react/static/media/mongodb.1f51812f.png"
+  },
+  {
+    "revision": "70cd7e78ce07660c097ba3305493f6d2",
+    "url": "/myPortfolio-react/static/media/node2.70cd7e78.png"
+  },
+  {
+    "revision": "99e2ca10f0f166c1d24f257be24ffe8e",
+    "url": "/myPortfolio-react/static/media/particules.99e2ca10.JPG"
+  },
+  {
+    "revision": "2b18b2b8565706e46a9b004d93187153",
+    "url": "/myPortfolio-react/static/media/portfolio-react.2b18b2b8.JPG"
+  },
+  {
+    "revision": "c06e6cff8c74b6d6f691d0874b52163d",
+    "url": "/myPortfolio-react/static/media/profilowe2.c06e6cff.png"
+  },
+  {
+    "revision": "25b05eca5f55915999af7d6997d82876",
+    "url": "/myPortfolio-react/static/media/react-apps.25b05eca.JPG"
+  },
+  {
+    "revision": "23cf7518256b960c4bedafb3ff25e389",
+    "url": "/myPortfolio-react/static/media/react.23cf7518.png"
+  },
+  {
+    "revision": "666a1f1f86f4267a62f088ae310c5659",
+    "url": "/myPortfolio-react/static/media/vsc.666a1f1f.png"
+  }
+]);
